2d5b640e0f06b711d16a5c1c8f34e1579341609c
Leonardo <leonardo.vitor@nexxera.com>
2016-03-07 14:35:53 -0300

    -add a deprecated annotation-add test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='deprecator',
+    version='0.0.1',
+    packages=['deprecator'],
+    url='https://github.com/xportation/deprecator',
+    license='',
+    author='leonardo',
+    author_email='xportation@gmail.com',
+    description='A deprecated annotation for python'
+)
---
+++ tests/deprecator_test.py
@@ -0,0 +1,42 @@
+import warnings
+from unittest import TestCase
+
+from deprecator.deprecator import deprecated
+
+
+class Foo:
+    @deprecated
+    def deprecated_method(self):
+        pass
+
+    def not_deprecated_method(self):
+        pass
+
+
+class DeprecatorTest(TestCase):
+
+    def setUp(self):
+        self.foo = Foo
+
+    def _assertWarns(self, warning, funct, isDeprecated):
+        with warnings.catch_warnings(record=True) as warning_list:
+            warnings.simplefilter('always')
+            funct(self.foo)
+            if isDeprecated:
+                self.assertTrue(any(item.category == warning for item in warning_list))
+            else:
+                self.assertFalse(any(item.category == warning for item in warning_list))
+
+    def test_call_deprecated_method(self):
+        self._assertWarns(
+            DeprecationWarning,
+            self.foo.deprecated_method,
+            True
+        )
+
+    def test_call_not_deprecated_method(self):
+        self._assertWarns(
+            DeprecationWarning,
+            self.foo.not_deprecated_method,
+            False
+        )
